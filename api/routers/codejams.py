625ad4cd19f84132144de8f34b919d94f74d123c
ks129 <45097959+ks129@users.noreply.github.com>
2021-07-19 12:42:51 +0300

    Remove unnecessary CJ fetching helper methods

--- api/routers/codejams.py
+++ api/routers/codejams.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-import asyncpg
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy import desc
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,53 +11,6 @@ from api.models import CodeJam, CodeJamResponse
 
 
 router = APIRouter(prefix="/codejams", tags=["codejams"])
-
-
-async def get_codejam_data(conn: asyncpg.Connection, jam_id: int, jam_name: str) -> dict[str, Any]:
-    """Gets all the data stored in the database about the specified codejam."""
-    codejam = {"jam_id": jam_id, "name": jam_name}
-
-    db_teams = await conn.fetch(
-        "SELECT team_id, team_name FROM teams WHERE jam_id = $1", jam_id
-    )
-
-    teams = []
-
-    for team_id, team_name in db_teams:
-
-        team = {}
-        users = []
-
-        db_users = await conn.fetch(
-            "SELECT user_id, is_leader from team_has_user WHERE team_id = $1",
-            team_id,
-        )
-
-        for user_id, is_leader in db_users:
-            users.append(dict(user_id=user_id, is_leader=is_leader))
-
-        team["users"] = users
-        team["name"] = team_name
-        team["team_id"] = team_id
-        teams.append(team)
-
-    codejam["teams"] = teams
-
-    codejam["infractions"] = [
-        dict(infraction)
-        for infraction in await conn.fetch(
-            "SELECT * FROM infractions WHERE jam_id = $1", jam_id
-        )
-    ]
-
-    codejam["winners"] = [
-        dict(winner)
-        for winner in await conn.fetch(
-            "SELECT user_id, first_place FROM winners WHERE jam_id = $1", jam_id
-        )
-    ]
-
-    return codejam
 
 
 @router.get("/", response_model=list[CodeJamResponse])
