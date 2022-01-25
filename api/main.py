9681700f448e9ebd944538bdd7f1e68fd8bcdc6a
ToxicKidz <itztoxickidz@gmail.com>
2022-01-24 20:29:59 -0500

    chore: Only use redoc for docs The default docs were accidentally added when testing

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from api.routers import codejams, infractions, users
 
 
-app = FastAPI(docs_url="/docs", redoc_url="/")
+app = FastAPI(redoc_url="/", docs_url=None)
 
 app.include_router(codejams.router)
 app.include_router(users.router)
