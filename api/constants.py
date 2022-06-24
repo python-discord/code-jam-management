132acce118bc4f0c311f645bf5912aa3aaf83e85
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2022-06-24 19:28:36 +0200

    Port Token-Authentication over from API, modify settings accordingly
    For reference see: https://github.com/python-discord/api/blob/main/api/core/middleware/token_authentication.py
    
    Co-Authored-By: Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>

--- api/constants.py
+++ api/constants.py
@@ -2,3 +2,5 @@ from decouple import config
 
 DATABASE_URL = config("DATABASE_URL")
 LOG_LEVEL = config("LOG_LEVEL", "INFO")
+DEBUG = config("DEBUG", cast=bool, default=False)
+TOKEN = config("API_TOKEN", cast=str, default="badbot13m0n8f570f942013fc818f234916ca531")
--- api/main.py
+++ api/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from starlette.middleware.authentication import AuthenticationMiddleware
 
+from api.constants import TOKEN
+from api.middleware import TokenAuthentication, on_auth_error
 from api.routers import codejams, infractions, teams, users, winners
 
-
 app = FastAPI(redoc_url="/", docs_url="/swagger")
+
+app.add_middleware(
+    AuthenticationMiddleware,
+    backend=TokenAuthentication(token=TOKEN),
+    on_error=on_auth_error,
+)
 
 app.include_router(codejams.router)
 app.include_router(infractions.router)
